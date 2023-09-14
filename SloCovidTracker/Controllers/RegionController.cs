dc72961fe3a369aecbfcb4e5cdd7c391676df0b9
Peter <ph2355@student.uni-lj.si>
2023-09-14 12:38:22 +0200

    Removed WeatherForecast.cs and WeatherForecastController.cs from demo app.

--- SloCovidTracker/Controllers/RegionController.cs
+++ SloCovidTracker/Controllers/RegionController.cs
@@ -14,12 +14,23 @@ public class RegionController : ControllerBase
         _logger = logger;
     }
 
+    /// <summary>
+    /// Get a list of daily cases, number of vaccinated 1st and 2nd to date, deaths to date. 
+    /// </summary>
+    /// <param name="regions"></param>
+    /// <param name="from"></param>
+    /// <param name="to"></param>
+    /// <returns></returns>
     [HttpGet("Cases")]
     public IEnumerable<DailyCasesByRegion> GetCases(List<string> regions, string from, string to)
     {
         return Enumerable.Empty<DailyCasesByRegion>();
     }
     
+    /// <summary>
+    /// Get average number of cases for each region in the last 7 days.
+    /// </summary>
+    /// <returns></returns>
     [HttpGet("LastWeek")]
     public IEnumerable<CasesLastWeek> GetLastWeek()
     {
