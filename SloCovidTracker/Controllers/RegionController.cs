02254cc51b017923cd19afb72f078b1859bb64fa
Peter <ph2355@student.uni-lj.si>
2023-09-14 12:10:05 +0200

    Added empty controllers.

---
+++ SloCovidTracker/Controllers/RegionController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using SloCovidTracker.Models;
+
+namespace SloCovidTracker.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RegionController : ControllerBase
+{
+    private readonly ILogger<RegionController> _logger;
+
+    public RegionController(ILogger<RegionController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("Cases")]
+    public IEnumerable<DailyCasesByRegion> GetCases(List<string> regions, string from, string to)
+    {
+        return Enumerable.Empty<DailyCasesByRegion>();
+    }
+    
+    [HttpGet("LastWeek")]
+    public IEnumerable<KeyValuePair<string, int>> GetLastWeek()
+    {
+        return Enumerable.Empty<KeyValuePair<string, int>>();
+    }
+}
